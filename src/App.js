7d89d1366193b210d7bea0155799c2ba999cc1e3
Sascha <hoffms.@gmail.com>
2021-08-04 15:27:02 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed.
+          Learn React Test
         </a>
       </header>
     </div>
